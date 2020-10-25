04402415aeee1685d647e310014c188fc618fb8c
Optix-Gamer <73211827+Optix-Gamer@users.noreply.github.com>
2020-10-25 16:50:58 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+client.login(process.env.NzY2MTQzMjE4MTQwMDUzNTA0.X4fEeQ.LTmGkISVbdkITnr5XxUJ6-3w3z0);
